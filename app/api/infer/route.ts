fde11eea5a9601590a23b530162046922ec9c3b0
aitwehrrg <rrginfinity@gmail.com>
2025-11-22 18:09:38 +0530

    Initial commit

---
+++ app/api/infer/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { text } = await req.json();
+
+  const res = await fetch(
+    `https://router.huggingface.co/hf-inference/models/${process.env.HF_USERNAME}/${process.env.HF_MODEL_NAME}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.HF_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputs: text }),
+    }
+  );
+
+  const data = await res.json();
+  return NextResponse.json(data);
+}
